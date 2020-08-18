5d0479d090514e5350e7876814a8736492632e5e
StacyGilbert7713 <63259897+StacyGilbert7713@users.noreply.github.com>
2020-08-17 21:21:53 -0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,36 +1,45 @@
 let coin = {
     state: 0,
-
     flip: function() {
-        this.state= Math.round(Math.random(0,1));
+        this.state = Math.floor(Math.random() * 2)
     },
-    
-    toString: function() {
-    
-        if(this.state === 0) {
-           return 'Heads'
-        }
 
-        if(this.state === 1) {
-           return 'Tails'
-        }      
+    toString: function() {
+        if (this.state === 0) {
+            return 'Heads'
+        } else {
+            return 'Tails'
+        }
     },
 
     toHTML: function() {
-  
-        let Heads = document.createElement('img');
-        let Tails = document.createElement('img');
-        Heads.src = './images/Heads.png'
-        Tails.src = './images/Tails.png'
-        Heads.style.height = '100px'
-        Tails.style.height = '100px'
-
+        let image = document.createElement('img');
         if (this.state === 0) {
-            return document.body.appendChild(Heads) 
+            image.src = "./images/heads.png"
+            image.style.width = '100px'
+        } else {
+            image.src = "./images/tails.png"
+            image.style.width = '100px'
         }
+        return image;
+    }
+}
 
-        if (this.state === 1) {
-            return document.body.appendChild(Tails)
-        }
-    },
-    
+    function display20Flips() {
+        for (let index = 0; index < 20; index ++) {
+            coin.flip()
+            let displayFlips = document.createElement('p')
+            displayFlips.append(coin.toString())
+            document.body.append(displayFlips)
+    }
+}
+
+    function display20Images() {
+        for (let index = 0; index < 20; index ++) {
+            coin.flip()
+            document.body.append(coin.toHTML())
+    }
+}
+
+display20Flips()
+display20Images()
